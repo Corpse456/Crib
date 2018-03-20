4e24ce092fcf20fe758d6781d31e73694d79c442
Alexander Neznaev <Corpsetrup@gmail.com>
2018-03-20 13:28:22 +0300

    create

--- src/main/java/dataBase/Create.sql
+++ src/main/java/dataBase/Create.sql
@@ -1,10 +1,12 @@
+CREATE SEQUENCE games_id_seq;
+
 CREATE TABLE games (
-	id int (5) AUTO_INCREMENT,
+	id int DEFAULT NEXTVAL('games_id_seq'),
 	name varchar (150) NOT NULL,
 	genre varchar (150),
 	date date NOT NULL,
 	rate_im dec (2,1),
 	rate_users dec (2,1),
-	link vachar NOT NULL,
+	link char NOT NULL,
 	PRIMARY KEY (id)
 );
