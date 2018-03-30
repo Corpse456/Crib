4386bad1c5bfe1097c84dcdf2d49a1e1e1fcc2e4
Alexander Neznaev <Corpsetrup@gmail.com>
2018-03-30 14:26:26 +0300

    create

--- src/main/java/dataBase/postgre/sqlQueries/Create.sql
+++ src/main/java/dataBase/postgre/sqlQueries/Create.sql
@@ -2,8 +2,8 @@ CREATE TABLE games (
 	id SERIAL PRIMARY KEY,
 	title varchar (150) NOT NULL,
 	genre varchar (150),
-	rel_date date NOT NULL,
-	rate_im dec (2,1),
-	rate_users dec (2,1),
+	rel_date date,
+	rate_im dec (3,1),
+	rate_users dec (3,1),
 	link text NOT NULL
 );
--- src/main/java/parsing/DateFormat.java
+++ src/main/java/parsing/DateFormat.java
@@ -3,6 +3,7 @@ package parsing;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateFormat {
     public static void main (String[] args) {
@@ -10,18 +11,18 @@ public class DateFormat {
         double d = 130;
         System.out.println(d != i);
         System.out.println(i);
-
+        
         System.out.println(Double.parseDouble("0."));
 
-        String s = "05 09 2013";
-        SimpleDateFormat format = new SimpleDateFormat("dd MM yyyy");
+        String s = "21 декабря 2000";
+        SimpleDateFormat format = new SimpleDateFormat("dd MMMM yyyy");
         Date docDate;
         try {
             docDate = format.parse(s);
         } catch (ParseException e) {
             docDate = new Date();
         }
-
-        System.out.println(docDate);
+        SimpleDateFormat format2 = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+        System.out.println(format2.format(docDate));
     }
 }
