f085467e8dd9573dd00a19ed90138c6c34d39b2f
Alexander Neznaev <Corpsetrup@gmail.com>
2018-04-12 18:07:26 +0200

    change table

--- src/main/java/dataBase/postgre/sqlQueries/CangeTable.sql
+++ src/main/java/dataBase/postgre/sqlQueries/CangeTable.sql
@@ -3,3 +3,5 @@ ALTER TABLE users RENAME TO userr;
 ALTER TABLE userr ALTER COLUMN id SET DEFAULT nextval('example_sequence');
 
 ALTER TABLE t_user rename COLUMN id TO user_id;
+
+ALTER TABLE public.t_book ALTER COLUMN date_prod DROP NOT NULL;
