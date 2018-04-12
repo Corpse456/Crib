59ad307552b39e07dc0992283feba82160d73119
Alexander Neznaev <Corpsetrup@gmail.com>
2018-04-12 21:57:20 +0200

    insert

--- src/main/java/dataBase/postgre/sqlQueries/CangeTable.sql
+++ src/main/java/dataBase/postgre/sqlQueries/CangeTable.sql
@@ -5,3 +5,5 @@ ALTER TABLE userr ALTER COLUMN id SET DEFAULT nextval('example_sequence');
 ALTER TABLE t_user rename COLUMN id TO user_id;
 
 ALTER TABLE public.t_book ALTER COLUMN date_prod DROP NOT NULL;
+
+ALTER TABLE t_autor add unique(autor_name);
--- src/main/java/dataBase/postgre/sqlQueries/Insert.sql
+++ src/main/java/dataBase/postgre/sqlQueries/Insert.sql
@@ -1,4 +1,6 @@
 INSERT INTO 
 	games(title, genre, rel_date, rate_im, rate_users, link )
 values
-	('game1', 'logic', '2018-Jan-08', '5', '6', 'goolge');
+	('game1', 'logic', '2018-Jan-08', '5', '6', 'goolge'),
+	('game2', 'logic', '2018-Jan-08', '4', '6', 'goolge'),
+	('game3', 'logic', '2018-Jan-08', '7', '6', 'goolge');
