14a721032bb8c5789490f99e5698462fd8460a10
Alexander Neznaev <Corpsetrup@gmail.com>
2018-05-24 13:34:20 +0300

    list files java 8

---
+++ src/main/java/files/ListFiles.java
@@ -0,0 +1,23 @@
+package files;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ListFiles {
+    
+    public static void main (String[] args) throws IOException {
+        long time = System.nanoTime();
+        List<File> collect = Files.walk(Paths.get("d:\\"))
+        .filter(Files::isRegularFile)
+        .map(Path::toFile)
+        .collect(Collectors.toList());
+        
+        System.out.println((System.nanoTime() - time) / 1000000000.0 + "| " + collect.size());
+        //collect.forEach(e -> System.out.println(e));
+    }
+}
