d05a7dd2c21b7658e8730a1d31ff63bc4481a544
Alexander Neznaev <corpsetrup@gmail.com>
2022-10-18 21:06:29 +0300

    add dbnames

---
+++ src/main/java/DbNames/AddBranchesFromFile.groovy
@@ -0,0 +1,9 @@
+package DbNames
+
+import groovy.json.JsonSlurper
+
+def inputFile = new File("SwAccountLocations2.js")
+def InputJSON = new JsonSlurper().parseText(inputFile.text)
+Collection collect = InputJSON.findAll { it.key != "Comments" }.collect { it.key }
+Map<String, String> env = System.getenv()
+collect.forEach{dbName -> AddNewBranch.createBranch(dbName, env['TEMP']) }
---
+++ src/main/java/DbNames/AddNewBranch.groovy
@@ -0,0 +1,18 @@
+package DbNames
+
+Map<String, String> env = System.getenv()
+createBranch(env['_DB_ACCOUNT'], env['TEMP'])
+
+static void createBranch(String accountName, String directory) {
+    print execute("git clone https://bitbucket.org/versonix/sw-jenkins-deploy", directory).text
+    directory += "/sw-jenkins-deploy"
+    String text = execute("git ls-remote --quiet --heads origin " + accountName, directory).text
+    if (text.isEmpty()) {
+        execute("git checkout -b " + accountName, directory)
+        execute("git push origin " + accountName, directory)
+    }
+}
+
+private static Process execute(String command, String directory) {
+    return command.execute(Collections.emptyList(), new File(directory))
+}
