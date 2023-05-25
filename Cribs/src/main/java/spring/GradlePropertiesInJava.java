2cc9aaafd79122bfbf7248750c2b8a66b33cab38
Alexander Neznaev <a.neznaev@invento-labs.com>
2023-05-25 16:56:13 +0300

    added buildProperties option

---
+++ Cribs/src/main/java/spring/GradlePropertiesInJava.java
@@ -0,0 +1,11 @@
+package spring;
+
+public class GradlePropertiesInJava {
+
+//  private final org.springframework.boot.info.BuildProperties buildProperties;
+
+//  springBoot  {
+//    buildInfo()
+//  }
+
+}
